7ca6c7782635ddf0c816b14f031746627350085b
HB <hussein_bakri@yahoo.com>
2018-02-03 17:22:28 +0000

    adding and testing Gulp

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+
+gulp.task('default', function(){
+  console.log("Hooray we have created a Gulp task how cool is that...");
+});
+
+gulp.task('html', function(){
+ console.log("Imagine something useful is being done to your HTML here...");
+
+});
